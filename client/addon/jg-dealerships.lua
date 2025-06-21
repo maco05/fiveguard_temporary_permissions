2ebca39af95bab0983689bb59f78e213b111f24c
maco05 <gosarmarcel7@gmail.com>
2025-06-21 11:19:22 +0200

    updated jg

--- client/addon/jg-dealerships.lua
+++ client/addon/jg-dealerships.lua
@@ -1,3 +1,7 @@
 AddEventHandler("jg-dealerships:client:open-showroom", function(dealershipId)
     TriggerServerEvent("jg-dealerships:enabletemppermissions")
 end)
+
+AddEventHandler("jg-dealerships:server:exit-showroom", function(dealershipId)
+    TriggerServerEvent("jg-dealerships:disabletemppermissions")
+end)
--- server/addon/jg-dealerships.lua
+++ server/addon/jg-dealerships.lua
@@ -10,16 +10,30 @@ if Config.JGDealerships == true then
             true,                        -- Allow?
             false                        -- Ignore static permissions
         )
-    end)
-
-    RegisterServerEvent("jg-dealerships:enabletemppermissions")
-    AddEventHandler("jg-dealerships:enabletemppermissions", function()
-        local src = source
-        exports[Config.FiveguardName]:SetTempPermission(
+         exports[Config.FiveguardName]:SetTempPermission(
             src,
             "Client",
             "BypassTeleport",
             true,
+            false
+        )
+    end)
+
+    RegisterServerEvent("jg-dealerships:disabletemppermissions")
+    AddEventHandler("jg-dealerships:disabletemppermissions", function()
+        local src = source
+        exports[Config.FiveguardName]:SetTempPermission(
+            src,                         -- Player source
+            "Client",                    -- Category
+            "BypassInvisible",         -- Permission
+            true,                        -- Allow?
+            false                        -- Ignore static permissions
+        )
+        exports[Config.FiveguardName]:SetTempPermission(
+            src,
+            "Client",
+            "BypassTeleport",
+            false,
             false
         )
     end)
