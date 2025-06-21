e1995feb0a8ebdc79041c2f8fb40156d2cc98022
maco05 <gosarmarcel7@gmail.com>
2025-06-21 09:30:04 -0700

    Update rcore_prison.lua

--- client/addon/rcore_prison.lua
+++ client/addon/rcore_prison.lua
@@ -2,6 +2,6 @@ AddEventHandler("rcore_prison:server:prologStarted", function()
     TriggerServerEvent("rcore_prison:enabletemppermissions")
 end)
 
-AddEventHandler("rcore_prison:server:prologStarted", function()
+AddEventHandler("rcore_prison:server:prologFinished", function()
     TriggerServerEvent("rcore_prison:disabletemppermissions")
 end)
