9166b2c2399c0ecb3d9f9993dcfc1537c98eb603
maco05 <gosarmarcel7@gmail.com>
2025-06-24 20:59:47 +0200

    Added option to wait x amout of seconds before disabling temporary permissions

--- client/client.lua
+++ client/client.lua
@@ -11,6 +11,14 @@ for key, Cfg in pairs(Config) do
             if Cfg.Debug then
                 print("[DEBUG] Stopping event triggered for resource:", Cfg.NameOfScript)
             end
+
+            if Cfg.WaitForStopping and tonumber(Cfg.WaitForStopping) and tonumber(Cfg.WaitForStopping) > 0 then
+                if Cfg.Debug then
+                    print("[DEBUG] Waiting for", tonumber(Cfg.WaitForStopping), "second(s) before disabling permissions.")
+                end
+                Citizen.Wait(tonumber(Cfg.WaitForStopping) * 1000)
+            end
+
             TriggerServerEvent(Cfg.NameOfScript .. ":disabletemppermissions")
         end)
     end
